95e6d05c1d6eecb21d1531fd1d7bca8f019a9128
taha <168944827+tahajam8304@users.noreply.github.com>
2024-05-12 00:35:32 +0330

    Update calcu.js

--- calcu.js
+++ calcu.js
@@ -5,10 +5,11 @@ const display = document.getElementById("display");
 function attendtodisplay(input) {
   display.value += input;
 }
-
+///equal
 function cleardisplay() {
   display.value = "";
 }
+///eval method
 
 function calculate() {
   display.value = eval(display.value);
